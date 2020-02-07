05a4c68616bf305aa15d1dc040ba8c9c8fe4635b
sudarshana <sudarshana@kpit.com>
2020-02-07 17:13:50 +0530

    Types modified

--- include/olctypes.h
+++ include/olctypes.h
@@ -29,19 +29,19 @@ using u_int_t = unsigned int;  //! unsigned int type specified
 /*!
  * Enumeration holds error details
  */
-typedef enum {
+using err_t = enum Errors {
   NO_ERROR,
   NO_ELEMENTS,
   NOT_FOUND,
   NULL_PTR,
   MAJOR_ERROR,
   MINOR_ERROR
-} err_t;
+};
 
 /*!
  * Structure holds Course details
  */
-typedef struct {
+using course_details_t = struct CourseDetails {
   course_id courseId;
   str_t courseName;
   str_t authorName;
@@ -49,38 +49,38 @@ typedef struct {
   u_int_t duration;
   u_int_t price;
   u_int_t courseRating;
-} CourseDetails_t;
+};
 
 /*!
  * Structure type holds User details
  */
-typedef struct {
+using user_details_t = struct UserDetails {
   usr_id userId;
   str_t userName;
   str_t fullName;
   str_t dateOfBirth;
   str_t domain;
-} UserDetails_t;
+};
 
 /*!
  * Structure to hold Vendor details
  */
-typedef struct {
+using vendor_details_t = struct VendorDetails {
   vendor_id vendorId;
   str_t vendorName;
   str_t fullName;
   str_t dateOfBirth;
   str_t domain;
-} VendorDetails_t;
+};
 
 /**
  * Types to define different lists
  */
-using CourseIdList = std::list<course_id>;  //! course id list type
-using UserIdList = std::list<course_id>;    //! user id list type
-using VendorIdList = std::list<course_id>;  //! vendor id list type
+using course_id_list_t = std::list<course_id>;  //! course id list type
+using user_id_list_t = std::list<course_id>;    //! user id list type
+using vendor_id_list_t = std::list<course_id>;  //! vendor id list type
 
-using GenericList =
+using generic_list_t =
     std::list<u_int_t>;  //! generic list type to hold different Ids
 
 #endif /* INCLUDE_OLCTYPES_H_ */
